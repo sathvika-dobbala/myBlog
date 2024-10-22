b297107b03a09b579183d2df165e667d82d170d7
Sathvika Dobbala <sathvidobbala@gmail.com>
2024-10-22 17:30:24 -0400

    all

--- app.py
+++ app.py
@@ -19,5 +19,10 @@ def resume():
 def projects():
     return render_template("projects.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
